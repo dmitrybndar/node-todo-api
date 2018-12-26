e971ae7aee725d543641abf6cde81a17abbae738
Dmitry Bondar <dmbd@Dmitrys-MacBook.local>
2018-12-26 22:07:12 +0200

    Add POST /todos route and mongoose models

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = mongoose.model('todos', TodoSchema);
