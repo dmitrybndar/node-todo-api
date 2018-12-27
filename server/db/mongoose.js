ab5b575e818b7a337d2579f31371aec80fe470cf
Dmitry Bondar <dmitry.bndar@gmail.com>
2018-12-27 02:42:27 +0200

    heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,10 @@ const url = process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(
   url,
   { useNewUrlParser: true },
-  () => {
+  err => {
+    if (err) {
+      return console.log(err);
+    }
     console.log('Connected to DB');
   }
 );
